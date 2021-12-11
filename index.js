7d5d9ee03951b966bfd2728982c306e9e60c2f37
Hrybeniuk Kateryna <dp040497see@gmail.com>
2021-12-11 18:00:39 +0200

    divided the function calc into 4 diferent functions

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+const firstNum = +prompt('Enter the first number'); 
+const secondNum = +prompt('Enter the second number');
+const mathFunct = prompt('Enter the math functions');
+let resultNum;
+
+if (mathFunct == "+") { 
+    sum(firstNum, secondNum);
+    resultNum = sum(firstNum, secondNum);
+    console.log (`Результат: ${firstNum} ${mathFunct} ${secondNum} = ${resultNum}`);    
+
+} else if (mathFunct == "-") {
+    subtraction (firstNum, secondNum);      
+    resultNum = subtraction(firstNum,secondNum);
+    console.log (`Результат: ${firstNum} ${mathFunct} ${secondNum} = ${resultNum}`);
+    
+} else if (mathFunct == "*") {
+    multiplication (firstNum, secondNum)
+    resultNum = multiplication(firstNum,secondNum);
+    console.log (`Результат: ${firstNum} ${mathFunct} ${secondNum} = ${resultNum}`);
+    
+} else if (mathFunct == "/") {
+    division (firstNum,secondNum);
+    resultNum = division(firstNum,secondNum);
+    console.log (`Результат: ${firstNum} ${mathFunct} ${secondNum} = ${resultNum}`);
+};
+
+
+function sum(first, second) {
+   return first + second;
+};
+function subtraction (first, second) {
+    return first - second;
+};
+function multiplication (first, second) {
+    return first * second;
+};
+
+function division (first, second) {
+    return first / second;
+};
+
+/*
 const firstNum = prompt('Enter the first number'); 
 const secondNum = prompt('Enter the second number');
 const mathFunct = prompt('Enter the math functions');
@@ -14,8 +56,4 @@ function calc() {
 };
 const resultNum = calc();
 console.log (`Результат: ${firstNum} ${mathFunct} ${secondNum} = ${resultNum}`)
-
-
-
-
-
+*/
